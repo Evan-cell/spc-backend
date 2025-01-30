5bc5ca8ac502bc86db6ed75ac4cb866a304297ca
Evan <evan@Evans-MacBook-Pro.local>
2025-01-30 12:40:35 +0300

    render config

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ import testRoute from "./routes/test.route.js"
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
+import path from 'path'
 
 const app = express();
 
@@ -18,6 +19,8 @@ app.use(
   })
 );
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -27,6 +30,12 @@ app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
+
+app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.listen(8000, () => {
   console.log("server is running!");
